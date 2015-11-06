3de8c82cb383930a90935042474c5eb84670f5fc
Mukhsin Arafat <mukhsin@outlook.com>
2015-11-06 14:49:35 +0700

    no message

---
+++ 04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,22 @@
+public class Profil {
+    
+    public String nama = "Mukhsin Arafat";
+    public String nim = "1408107010073";
+    
+    public Profil(){
+    }
+    
+    public Profil (String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getNim() {
+        return nim;
+    }
+}
+  
